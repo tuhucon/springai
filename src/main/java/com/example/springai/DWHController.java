b1373d061a652e169b123be06ceb6169f11c9d3d
tuhucon <minhtuan93@yahoo.com>
2025-03-27 10:47:32 +0700

    add 2 fake AI tool, raw question can be passed into tool, tool can return directly without LLM in the middle

---
+++ src/main/java/com/example/springai/DWHController.java
@@ -0,0 +1,37 @@
+package com.example.springai;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DWHController {
+
+    @GetMapping("/dwh")
+    @Tool(
+            description = """
+                    connect to data warehouse, implement query and generate report.
+                    data warehouse: DWH, dwh.
+                    AI need to pass the raw question of user into query parameter.
+                    """,
+            returnDirect = true
+    )
+    public String dwh(@ToolParam(description = "the raw question of the users") String query) {
+        return  "dwh - response of: " + query;
+    }
+
+    @Tool(
+            description = """
+                    connect to customer services, implement query and generate report.
+                    customer services: CS
+                    AI need to pass the raw question of user into query parameter
+                    """,
+            returnDirect = true
+    )
+    public String customer(@ToolParam(description = "the raw question of the users") String query) {
+        return  "customer - response of: " + query;
+    }
+}
