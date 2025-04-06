17c953f0ab279ddf1b547ac7a6624775a305c2cf
tuhucon <minhtuan93@yahoo.com>
2025-04-06 20:28:31 +0700

    pass context into tool tool call history is also passed into tool via context

---
+++ src/main/java/com/example/springai/ToolBeanConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.springai;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+
+import java.util.function.Function;
+
+@Configuration
+public class ToolBeanConfiguration {
+
+    public static final String GET_INTEREST = "getInterest";
+
+    @Bean(GET_INTEREST)
+    @Description("truy vấn và trả về số tiền lãi của một user khi chơi chứng khoán")
+    Function<String, Double> getInterest() {
+        return Double::parseDouble;
+    }
+}
--- src/main/java/com/example/springai/ToolController.java
+++ src/main/java/com/example/springai/ToolController.java
@@ -2,12 +2,14 @@ package com.example.springai;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ToolContext;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +20,10 @@ public class ToolController {
     public static class DateTimeTool {
 
         @Tool(description = "provide the current date and time")
-        public String getCurrentDateTime() {
+        public String getCurrentDateTime(ToolContext context) {
             System.out.println("************run method getCurrentDateTime****************");
+            System.out.println("context: " + context.getContext());
+            System.out.println("tool call history: " + context.getToolCallHistory());
             return LocalDateTime.now().toString();
         }
     }
@@ -30,6 +34,7 @@ public class ToolController {
         return chatClient
                 .prompt("bây giờ là mấy giờ")
                 .tools(new DateTimeTool())
+                .toolContext(Map.of("workspace id", 1234, "workspace name", "tu hu con"))
                 .call()
                 .content();
     }
